db02a6a9882a74ed144ef6c09d0c3b24353ca573
AryanKothari <kothari.s.aryan@gmail.com>
2024-12-14 01:41:45 +0530

    testing with minor update

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -22,7 +22,7 @@ const teamMembers = [
   },
   {
     name: "Aryan Kothari",
-    role: "SMART Component",
+    role: "Computer Science Major",
     bio: "Creative designer focused on crafting beautiful and intuitive user experiences.",
     image: "https://media.licdn.com/dms/image/v2/D4D03AQHaxEsgGMHZ9A/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1727707160195?e=1738800000&v=beta&t=WMSTGmQ9r_ww_RapvHM7Blskb_eRFXT1fGJHtkvl89E",
     link: "https://www.linkedin.com/in/akothari12/"
